73e396d9c6e1f1a212daaf042f6611dd00de0c9f
Mayank Raj <rajmay@sheridancollege.ca>
2023-05-26 09:42:38 -0400

    Added "Hi"

---
+++ src/arraydemos23friam/PartTimeStudent.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemos23friam;
+
+/**
+ *
+ * @author mayankraj
+ */
+public class PartTimeStudent {
+    
+    
+    
+}
